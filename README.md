9f4c65c6ffb48723199f37a15d0c708d7f139065
Jay Joshi <jayjosh890@gmail.com>
2023-04-17 23:57:31 -0400

    Added more details

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Lambda-VPCLookup
+
+This program has a python Lambda function which extracts 
+
+
+This function assumes that you have an IAM role named "RoleName" in each AWS account that has permissions to retrieve details about VPCs. You will need to replace this with the appropriate IAM role ARN for each account. Additionally, you may need to modify the IAM permissions for the role to allow the lambda function to assume the role and access the necessary resources.
--- lambda-vpc-lookup-s3.py
+++ lambda-vpc-lookup-s3.py
@@ -1,37 +1,56 @@
 import boto3
 import json
+import datetime
 
 def lambda_handler(event, context):
-    # Set up S3 client
-    s3 = boto3.client('s3')
-    bucket_name = "target"
     
-    # Set up IAM client to list all accounts
-    iam = boto3.client('iam')
-    account_list = iam.list_account_aliases()['AccountAliases']
+    # Define a list to store the VPC information
+    vpc_info = []
     
-    # Set up EC2 client to retrieve VPC details
+    # Initialize an EC2 client using the temporary credentials
     ec2 = boto3.client('ec2')
+        
+    # Retrieve all VPCs in the account
+    response = ec2.describe_vpcs()
+        
+    # Loop through each VPC in the response
+    for vpc in response['Vpcs']:
+        vpc_id = vpc['VpcId']
+        vpc_cidr = vpc['CidrBlock']
+        vpc_tags = vpc.get('Tags', [])
+            
+        # Retrieve all subnets in the VPC
+        subnet_response = ec2.describe_subnets(Filters=[{'Name': 'vpc-id', 'Values': [vpc_id]}])
+        subnets = []
+            
+        # Loop through each subnet in the response
+        for subnet in subnet_response['Subnets']:
+            subnet_id = subnet['SubnetId']
+            subnet_cidr = subnet['CidrBlock']
+            subnet_tags = subnet.get('Tags', [])
+                
+            subnets.append({
+                'id': subnet_id,
+                'cidr': subnet_cidr,
+                'tags': subnet_tags
+            })
+            
+        vpc_info.append({
+            'id': vpc_id,
+            'cidr': vpc_cidr,
+            'tags': vpc_tags,
+            'subnets': subnets
+        })
     
-    # Loop through all accounts and retrieve VPC details
-    for account in account_list:
-        # Assume role in account
-        sts = boto3.client('sts')
-        assumed_role_object = sts.assume_role(RoleArn='arn:aws:iam::' + account + ':role/RoleName', RoleSessionName="AssumeRoleSession1")
-        credentials = assumed_role_object['Credentials']
-        ec2 = boto3.client('ec2', aws_access_key_id=credentials['AccessKeyId'],
-                            aws_secret_access_key=credentials['SecretAccessKey'],
-                            aws_session_token=credentials['SessionToken'])
-        
-        # Retrieve VPC details
-        response = ec2.describe_vpcs()
-        
-        # Dump VPC details to S3
-        for vpc in response['Vpcs']:
-            key_name = 'account-' + account + '-vpc-' + vpc['VpcId'] + '.json'
-            s3.put_object(Bucket=bucket_name, Key=key_name, Body=json.dumps(vpc))
+    # Dump the VPC information to an S3 bucket
+    bucket_name = '{mybucketname}'
+    s3_key = 'vpc_info_{}.json'.format(datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
+    
+    s3 = boto3.resource('s3')
+    object = s3.Object(bucket_name, s3_key)
+    object.put(Body=json.dumps(vpc_info))
     
     return {
         'statusCode': 200,
-        'body': json.dumps('VPC details dumped successfully to S3!')
+        'body': json.dumps('VPC information dumped to S3 bucket!')
     }
